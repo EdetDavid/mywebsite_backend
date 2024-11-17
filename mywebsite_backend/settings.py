3778d3ca30106baa027003b2da9f2cefdb39279e
Dvooskid <dvooskid1234@gmail.com>
2024-11-17 15:15:17 +0100

    Update settings.py

--- mywebsite_backend/settings.py
+++ mywebsite_backend/settings.py
@@ -79,26 +79,13 @@ WSGI_APPLICATION = "mywebsite_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-"""
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'verceldb',
-        'USER':   'default',
-        'PASSWORD': 'E4bhj0Mqnmda',
-        'HOST': 'ep-lively-lake-a2jlb0qt-pooler.eu-central-1.aws.neon.tech',
-        'PORT': '5432'
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
-
 }
-"""
 
- DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
-              } 
-           } 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
