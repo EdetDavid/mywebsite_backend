117fb3fca7dc6e127abd716dd89c6b06dc5476e4
Dvooskid <dvooskid1234@gmail.com>
2024-11-17 06:45:15 +0100

    Update settings.py

--- mywebsite_backend/settings.py
+++ mywebsite_backend/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = "mywebsite_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -90,12 +91,13 @@ DATABASES = {
     }
 
 }
+"""
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': "db.sqlite3"
-#     }}
+ DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': "db.sqlite3"
+     }}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
